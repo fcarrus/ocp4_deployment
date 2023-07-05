34ee2918f128192825b5466bf7c5b8209822c2fb
Fulvio Carrus <fcarrus@redhat.com>
2023-07-05 18:11:04 +0200

    Fix run.sh example

--- run.sh
+++ run.sh
@@ -2,19 +2,16 @@
 
 # Sample script to help with launching the automation workflow.
 
-# Requirements for the machine where the workflow is ran:
+# Requirements for the machine where the workflow is run:
 # * Install podman : yum install podman
 
 set -x
 
+# Set this directory to the one you created on the machine.
 MYDIRECTORY="/home/myuser/my-ocp-installation-files"
 
 IMAGE=quay.io/fcarrus/ocp4-deployment-automation:latest
-
-VOLUMES="
--v ${MYDIRECTORY}/:/ocp4-deployment/:Z
-"
-
+VOLUMES="-v ${MYDIRECTORY}/:/ocp4-deployment/:Z"
 PLAYBOOK=${1:-main}
 
 shift
